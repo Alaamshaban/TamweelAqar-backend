2b91a89388f03dfb226cddb337b9d51342332e70
Alaa SHABAAN <alaa.shabaan@valeo.com>
2021-10-22 22:43:50 +0200

    add add-view to offers

--- src/routes/getIndividualOffer.js
+++ src/routes/getIndividualOffer.js
@@ -1,12 +1,18 @@
 import { Offers } from "./offers";
 import Boom from '@hapi/boom';
+import { db } from '../database';
 
 export const getOfferRoute = {
     method: 'GET',
     path: '/api/offers/{id}',
-    handler: (req, h) => {
+    handler: async (req, h) => {
         const id = req.params.id;
-        const offer = Offers.find(offer => offer.id === id);
+        //   const offer = Offers.find(offer => offer.id === id);
+        const { results } = await db.query(
+            'SELECT * FROM offers WHERE id=?',
+            [id]
+        )
+        const offer = results[0];
         if (!offer) {
             throw Boom.notFound(`Offer is not existing with id ${id}`)
         }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
+import { addViewToOffer } from "./addViewToOffer";
 import { getOfferRoute } from "./getIndividualOffer";
 import { getOffersRoute } from "./getOffersRoute"
 
 export default[
     getOffersRoute,
-    getOfferRoute
+    getOfferRoute,
+    addViewToOffer
 ];
