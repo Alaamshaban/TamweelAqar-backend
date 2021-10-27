9f5f6a91c183b6e98e357cf98e8adfe9a759f893
Alaa SHABAAN <alaa.shabaan@valeo.com>
2021-10-28 01:23:22 +0200

    add calculations of mortgage

--- src/routes/getOffersRoute.js
+++ src/routes/getOffersRoute.js
@@ -1,15 +1,30 @@
 import { Offers } from "./offers";
-import {db} from '../database';
+import { db } from '../database';
+import PMT from 'formula-pmt';
 
 export const getOffersRoute = {
     method: 'GET',
-    path: '/api/offers',
-    handler:async (req, h) => {
+    path: '/api/offers/',
+    handler: async (req, h) => {
         //  return Offers;
+        const { purchase_price, user_salary, down_payment, mortgage_term_length } = req.query;
 
-        const { results } = await db.query(
+        const { results } = await db.query('SELECT interest_rate,mortgage_term_length,purchase_price,down_payment,monthly_payment FROM offers');
+
+        for (let i = 0; i < results.length; i++) {
+            const mortgage_amount = results[i].purchase_price * (1 - (results[i].down_payment / 100));
+            console.log(mortgage_amount, results[i].mortgage_term_length, results[i].interest_rate)
+            await db.query(
+                `UPDATE offers SET monthly_payment =
+                 ${Math.abs(PMT(results[i].interest_rate / 100 / 12, results[i].mortgage_term_length * 12, mortgage_amount, 0, 0))}
+                 WHERE
+                 id = ${i + 1};`
+            )
+        }
+
+        const x = await db.query(
             'SELECT * FROM offers'
         );
-        return results;
+        return x.results;
     }
 }
--- src/routes/getUserOffers.js
+++ src/routes/getUserOffers.js
@@ -4,25 +4,36 @@ import Boom from '@hapi/boom';
 
 export const getUserOffers = {
     method: 'GET',
-    path: '/api/users/{user_id}/offers',
+    path: '/api/users/{user_id}/offers/',
     handler: async (req, h) => {
         const token = req.headers.authtoken;
         console.log('token??', token);
         try {
             const user = await admin.auth().verifyIdToken(token, true);
-            // // admin.auth().verifyIdToken(token, true).then(user => {
-            // //     console.log(user)
-            // // })
-            // console.log(user)
             if (user) {
-                const { results } = await db.query(
+                const { purchase_price, user_salary, down_payment, mortgage_term_length } = req.query;
+
+                const { results } = await db.query('SELECT interest_rate,mortgage_term_length,purchase_price,down_payment,monthly_payment FROM offers');
+
+                for (let i = 0; i < results.length; i++) {
+                    const mortgage_amount = results[i].purchase_price * (1 - (results[i].down_payment / 100));
+                    console.log(mortgage_amount, results[i].mortgage_term_length, results[i].interest_rate)
+                    await db.query(
+                        `UPDATE offers SET monthly_payment =
+                         ${Math.abs(PMT(results[i].interest_rate / 100 / 12, results[i].mortgage_term_length * 12, mortgage_amount, 0, 0))}
+                         WHERE
+                         id = ${i + 1};`
+                    )
+                }
+                const x = await db.query(
                     'SELECT * FROM offers',
                 );
-                return results;
+                return x.results;
             }
-         //   throw Boom.unauthorized('Users can only access offers, Please Sign in first');
-        } catch(err) {
-            console.log(err)
+            //   throw Boom.unauthorized('Users can only access offers, Please Sign in first');
+        } catch (err) {
+            console.log(err);
+            admin.auth().revokeRefreshTokens
             throw Boom.unauthorized('Users can only access offers, Please Sign in first');
         }
 
