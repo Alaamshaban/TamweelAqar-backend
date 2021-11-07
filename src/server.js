6fe915c87b824c371f45ea3f22390d0e76df561b
Alaa SHABAAN <alaa.shabaan@valeo.com>
2021-11-07 20:41:04 +0200

    set origin to ignore

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ const start = async () => {
             ...route,
             options: {
                 cors: {
-                    origin: ['*'],
+                    origin: ['ignore'],
                     credentials: true
                 },
             }
