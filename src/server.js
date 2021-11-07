51e7cfc3f121de31da32e0b56c148558980bd443
Alaa SHABAAN <alaa.shabaan@valeo.com>
2021-11-07 21:49:28 +0200

    remove host

--- src/server.js
+++ src/server.js
@@ -14,8 +14,8 @@ let myServer;
 
 const start = async () => {
     myServer = Hapi.server({
-        host: '0.0.0.0/0',
-        port: process.env.PORT || 1337,
+        port: process.env.PORT || 3000,
+        labels: ['api'],
         routes: {
             cors: {
                 origin: ['*'], // an array of origins or 'ignore'    
