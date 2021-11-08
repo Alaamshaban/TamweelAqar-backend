b8f4708d35aeddde7e30d2804bd62a0b04677ec3
Alaa SHABAAN <alaa.shabaan@valeo.com>
2021-11-08 12:16:34 +0200

    remove maxAge

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const start = async () => {
                 headers: ['AuthToken'], // an array of strings - 'Access-Control-Allow-Headers'
                 exposedHeaders: ['Accept'], // an array of exposed headers - 'Access-Control-Expose-Headers',
                 additionalExposedHeaders: ['Accept'], // an array of additional exposed headers
-                maxAge: 60,
+                // maxAge: 60,
                 credentials: true
             }
         }
