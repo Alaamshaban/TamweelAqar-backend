5a95be2c1b51ef7c4b0ab48f5a7319bcdd538e0b
Alaa SHABAAN <alaa.shabaan@valeo.com>
2021-11-30 10:32:58 +0200

    update user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,43 +1,20 @@
 const {  DataTypes } = require('sequelize');
 export const UserModel = {
     // Model attributes are defined here
-    user_name: {
-        type: DataTypes.STRING
-    },
-    user_id: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        primaryKey: true
-    },
     full_name: {
         type: DataTypes.STRING,
         allowNull: false
     },
     phone_number: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        primaryKey: true
     },
     email_address: {
         type: DataTypes.STRING,
         allowNull: false
     },
-    token: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    password: {
-        type: DataTypes.STRING
-    },
-    gender: {
-        type: DataTypes.STRING
-    },
-    date_of_birth: {
-        type: DataTypes.STRING
-    },
-    address: {
-        type: DataTypes.STRING
-    },
-    nationality: {
+    user_name: {
         type: DataTypes.STRING
     },
     employment_status: {
