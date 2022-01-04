983d406e729bf2932b94c702730244e677d0e74e
Alaa SHABAAN <alaa.shabaan@valeo.com>
2022-01-04 23:02:34 +0200

    add created_at to revealed offers and post user

--- src/routes/addViewToOffer.js
+++ src/routes/addViewToOffer.js
@@ -6,30 +6,31 @@ export const addViewToOffer = {
     method: 'POST',
     path: '/api/offers/{id}/reveal',
     handler: async (req, h) => {
+        console.log('///////////////')
         const token = req.headers.authtoken;
-        console.log('token??', token);
-        const { user_id = '', interest_rate = '', monthly_payment = '', revealed_by = '' } = req.payload;
+        console.log('token????', token);
+        const { user_id = '', interest_rate = '', monthly_payment = '', revealed_by = '', admin_fees = '', mortgage_term_length = '', created_at = '',down_payment='' } = req.payload;
         const offer_id = req.params.id;
         try {
             const user = await admin.auth().verifyIdToken(token, true);
             if (user) {
                 await db.query(
-                    `REPLACE INTO revealed_offers (user_id,interest_rate, monthly_payment,revealed_by,offer_id)
-                    VALUES (?,?,?,?,?)`,
-                    [user_id, interest_rate, monthly_payment, revealed_by,offer_id]
+                    `REPLACE INTO revealed_offers (user_id,interest_rate, monthly_payment,revealed_by,offer_id,admin_fees,created_at,mortgage_term_length,down_payment)
+                    VALUES (?,?,?,?,?,?,?,?,?)`,
+                    [user_id, interest_rate, monthly_payment, revealed_by, offer_id, admin_fees, created_at, mortgage_term_length,down_payment]
                 )
                 await db.query(
                     'UPDATE offers SET views=views+1 WHERE id=?',
                     [offer_id]
                 )
-                const  {results}  = await db.query(
+                const { results } = await db.query(
                     'SELECT * FROM revealed_offers'
                 )
                 return results;
             }
         } catch (err) {
             console.log(err);
-            throw Boom.unauthorized('Users can only access offers, Please Sign in first');
+            throw Boom.badRequest('An error accured please try again');
         }
     }
 }
--- src/routes/postUser.js
+++ src/routes/postUser.js
@@ -6,13 +6,13 @@ export const addUser = {
     method: 'POST',
     path: '/api/users',
     handler: async (req, h) => {
-        const { user_name = '', full_name = '', phone_number = '', token = '', user_id = '', email_address = null } = req.payload;
+        const { user_name = '', full_name = '', phone_number = '', token = '', user_id = '', email_address = null,registered_at='' } = req.payload;
         console.log(user_id, token)
         if (user_id !== null && user_id !== '') {
             await db.query(
-                `INSERT INTO users (user_name,full_name, phone_number,token,user_id,email_address)
-                VALUES (?,?,?,?,?,?);`,
-                [user_name, full_name, phone_number, token, user_id, email_address]
+                `INSERT INTO users (user_name,full_name, phone_number,token,user_id,email_address,registered_at)
+                VALUES (?,?,?,?,?,?,?);`,
+                [user_name, full_name, phone_number, token, user_id, email_address,registered_at]
             )
             const { results } = await db.query(
                 `SELECT * FROM users WHERE user_id='${user_id}'`,
